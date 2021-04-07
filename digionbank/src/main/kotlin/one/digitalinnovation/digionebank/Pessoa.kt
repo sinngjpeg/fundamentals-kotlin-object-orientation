f21937f524fa0661eb241e1561d4856c2df6f604
Ingrid Silva <ingridsilvatec@outlook.com>
2021-04-07 00:54:07 -0300

    utiliza inner class

--- digionbank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ digionbank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,10 +3,15 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Ingrid"
     var cpf: String = "123.459.145-88"
+    
+    inner class Endereco {
+        val rua: String = "Rua Andorinhas"
+    }
 }
 
 fun main() {
     val ingrid = Pessoa()
     println(ingrid.nome)
     println(ingrid.cpf)
+    print(ingrid.Endereco().rua)
 }
