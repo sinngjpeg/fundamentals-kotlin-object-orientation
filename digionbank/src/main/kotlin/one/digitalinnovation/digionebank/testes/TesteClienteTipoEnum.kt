8fa2b8b4c2480cc0ba0696159dd2a4e8c226ac75
Ingrid Silva <ingridsilvatec@outlook.com>
2021-04-07 02:04:00 -0300

    cria classe enum

---
+++ digionbank/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteClienteTipoEnum.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.ClienteTipo
+
+fun main() {
+    ClienteTipo.values().forEach { elemento ->
+        println("${elemento.name} ${elemento.descricao}")
+    }
+
+    ClienteTipo.values().forEach {
+        println("${it.name} ${it.descricao}")
+    }
+}
