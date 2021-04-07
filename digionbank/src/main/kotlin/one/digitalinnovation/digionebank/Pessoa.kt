4ada738ad8cdf97fdecf678a625004264b399b19
Ingrid Silva <ingridsilvatec@outlook.com>
2021-04-07 01:01:22 -0300

    implementa o private set

--- digionbank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ digionbank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,15 +3,13 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Ingrid"
     var cpf: String = "123.459.145-88"
-    
-    inner class Endereco {
-        val rua: String = "Rua Andorinhas"
-    }
+        private set
+
 }
 
 fun main() {
     val ingrid = Pessoa()
     println(ingrid.nome)
     println(ingrid.cpf)
-    print(ingrid.Endereco().rua)
+
 }
