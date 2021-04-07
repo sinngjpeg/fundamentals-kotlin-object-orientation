5163b3bd1b343f462a094d50cecb70307422d55b
Ingrid Silva <ingridsilvatec@outlook.com>
2021-04-07 02:06:28 -0300

    cria instancia de enum no teste

--- digionbank/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteClienteTipoEnum.kt
+++ digionbank/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteClienteTipoEnum.kt
@@ -10,4 +10,11 @@ fun main() {
     ClienteTipo.values().forEach {
         println("${it.name} ${it.descricao}")
     }
+
+    val pf = ClienteTipo.PF
+    println(">>${pf.name} ${pf.descricao}")
+
+    val pj = ClienteTipo.PF
+    println(">>${pj.name} ${pj.descricao}")
+
 }
