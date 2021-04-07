2e49c7750cf4331d3c02818c5595c777b3271695
Ingrid Silva <ingridsilvatec@outlook.com>
2021-04-07 00:42:28 -0300

    cria classe pessoa, testa instanciamento

--- digionbank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ digionbank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,4 +1,12 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String = "Ingrid"
+    var cpf: String = "123.459.145-88"
+}
+
+fun main() {
+    val ingrid = Pessoa()
+    println(ingrid.nome)
+    println(ingrid.cpf)
 }
