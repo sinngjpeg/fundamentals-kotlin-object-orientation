e32bbea058bd6d123a64d6d00d3d74610a604b15
Ingrid Silva <ingridsilvatec@outlook.com>
2021-04-12 21:50:34 -0300

    cria teste IntArrayOf

---
+++ api-collections/src/main/kotlin/sinngjpeg/collections/TesteIntArrayOf.kt
@@ -0,0 +1,16 @@
+package sinngjpeg.collections
+
+fun main() {
+    val values = intArrayOf(2, 3, 4, 1, 10, 7)
+
+    values.forEach {
+        println(it)
+    }
+
+    print("------------")
+
+    values.sort()
+    values.forEach {
+        println(it)
+    }
+}
