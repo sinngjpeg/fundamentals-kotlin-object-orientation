63e18bd9f89aa6f258e2d8a1d64779b0827cfb8a
Ingrid Silva <ingridsilvatec@outlook.com>
2021-04-14 21:11:52 -0300

    cria teste operacoes max(), min(), average(), filter

---
+++ api-collections/src/main/kotlin/sinngjpeg/collections/TesteOperacoes.kt
@@ -0,0 +1,19 @@
+package sinngjpeg.collections
+
+fun main() {
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+
+    for (salario in salarios) {
+        println(salario)
+    }
+
+    println("---------------")
+    println("Maior salario: ${salarios.max()}")
+    println("Menor salario: ${salarios.min()}")
+    println("Media salarial: ${salarios.average()}")
+
+    val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
+    println("---------------")
+    salariosMaiorQue2500.forEach { println(it) }
+
+}
