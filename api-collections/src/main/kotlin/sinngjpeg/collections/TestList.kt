f2cbdec34693680712108af322b404538016baa6
Ingrid Silva <ingridsilvatec@outlook.com>
2021-04-14 22:02:13 -0300

    cria teste operacao sortedby

--- api-collections/src/main/kotlin/sinngjpeg/collections/TestList.kt
+++ api-collections/src/main/kotlin/sinngjpeg/collections/TestList.kt
@@ -2,8 +2,8 @@ package sinngjpeg.collections
 
 fun main() {
 
-    val joao = Funcionario("Joao", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
+    val joao = Funcionario("Joao", 2000.0)
+    val pedro = Funcionario("Pedro", 1500.0)
     val maria = Funcionario("Maria", 4000.0)
 
     val funcionarios = listOf(joao, pedro, maria)
@@ -11,6 +11,12 @@ fun main() {
 
     println("-------------")
     println(funcionarios.find { it.nome == "Maria" })
+
+    println("-------------")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
 }
 
 data class Funcionario(
