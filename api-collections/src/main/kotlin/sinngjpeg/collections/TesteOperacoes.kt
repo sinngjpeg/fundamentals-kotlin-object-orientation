f351143ee3ced7da6edec50d78635d6e8a589447
Ingrid Silva <ingridsilvatec@outlook.com>
2021-04-14 21:24:16 -0300

    cria teste de operacoes find, any

--- api-collections/src/main/kotlin/sinngjpeg/collections/TesteOperacoes.kt
+++ api-collections/src/main/kotlin/sinngjpeg/collections/TesteOperacoes.kt
@@ -16,4 +16,15 @@ fun main() {
     println("---------------")
     salariosMaiorQue2500.forEach { println(it) }
 
+    println("---------------")
+    println(salarios.count { it in 2000.0..5000.0 })
+
+    println("---------------")
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find {it == 500.0})
+
+    println("---------------")
+    println(salarios.any {it == 1000.0})
+    println(salarios.any {it == 500.0})
+
 }
