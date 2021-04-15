31dd230b0bff9436c867f2264131aff73d6bb6be
Ingrid Silva <ingridsilvatec@outlook.com>
2021-04-14 22:04:37 -0300

    cria teste operacao groupby

--- api-collections/src/main/kotlin/sinngjpeg/collections/TestList.kt
+++ api-collections/src/main/kotlin/sinngjpeg/collections/TestList.kt
@@ -2,9 +2,9 @@ package sinngjpeg.collections
 
 fun main() {
 
-    val joao = Funcionario("Joao", 2000.0)
-    val pedro = Funcionario("Pedro", 1500.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("Joao", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
     funcionarios.forEach { println(it) }
@@ -17,11 +17,17 @@ fun main() {
         .sortedBy { it.salario }
         .forEach { println(it) }
 
+    println("-------------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
+
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ) {
     override fun toString(): String =
         """
