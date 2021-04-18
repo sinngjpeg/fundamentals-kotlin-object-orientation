ee0318770d7b3d56832c85e5ef8e4dc5df3d46e8
Ingrid Silva <ingridsilvatec@outlook.com>
2021-04-18 20:48:44 -0300

    testa generics

---
+++ api-collections/src/main/kotlin/sinngjpeg/collections/TesteMutavelMap.kt
@@ -0,0 +1,15 @@
+package sinngjpeg.collections
+
+fun main() {
+    val joao = Funcionario("Joao", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val repositorio = Repositorio<Funcionario>()
+
+    repositorio.create(joao.nome, joao)
+    repositorio.create(pedro.nome, pedro)
+    repositorio.create(maria.nome, maria)
+
+    println(repositorio.findById(joao.nome))
+}
